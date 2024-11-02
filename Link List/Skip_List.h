2ab710d2e89478cb0a496d14f6c697553ccdd027
jlu005807 <1289406660@qq.com>
2024-11-02 23:37:25 +0800

    Skip_List

--- Link List/Skip_List.h
+++ Link List/Skip_List.h
@@ -7,7 +7,7 @@
 //引入了 分层 的概念。首先，跳表的每一层都是一个有序链表,特别地，最底层是初始的有序链表
 
 
-//定义跳表节点模板，每个节点为一组键值<key,valve>,在List中key不重复
+//定义跳表节点模板，每个节点为一组键值<key,value>,在List中key不重复
 template<class K,class V>
 class SkipListNode
 {
@@ -16,13 +16,14 @@ public:
 
 	K key;//键
 
-	V valve;//值
+	V value;//值
 
-	SkipListNode** forward;//指向下一层的指针数组
+	SkipListNode** forward;//指向各层的指针数组
 
 	//构造函数
 	SkipListNode() = default;
 
+	//默认K,V有默认构造函数，并且K支持<运算符
 	SkipListNode(K k, V v, int l, SkipListNode* next=nullptr) :key(k), value(v), level(l),forward(nullptr)
 	{
 		//构建指针数组
@@ -72,7 +73,7 @@ public:
 	SkipListNode<K, V>* head;
 	SkipListNode<K, V>* tail; 
 
-	//表中元素数量
+	//跳表的长度，即表中元素数量
 	int length;
 
 	//当前跳表层数
@@ -112,15 +113,146 @@ public:
 		
 	}
 
+	//查询跳表中是否存在键值为 key 的节点。若存在返回Value
+	V& find(const K& key)
+	{
+		//获得哨兵结点
+		SkipListNode<K, V>* p = head;
+
+		//遍历，找到该层最后一个键值小于 key 的节点，然后走向下一层
+		for (int i = level; i >= 0; i--)
+		{
+			//找到该层最接近Key并小于Key的节点
+			while (p->forward[i]->key < key)
+			{
+				p = p->forward[i];//前移
+			}
+		}
+
+		// 现在是小于，所以还需要再往后走一步
+		p = p->forward[0];
+
+		//现在p->key大于或者等于key,需要再判断
+		//找到节点
+		if (p->key == key)
+		{
+			return p->key;
+		}
+
+		// 节点不存在，返回 INVALID
+		return tail->value;
+	}
+
+
 	//插入键值为<key,value>的元素
+	//先执行一遍查询的过程，中途记录新节点是要插入哪一些节点的后面，最后再执行插入。
+	//每一层最后一个键值小于 key 的节点，就是需要进行修改的节点。
 	void Insert(const K& key, const V& value)
 	{
 		//用于记录需要更新的节点
 		SkipListNode<K, V>* updata[MAX_LEVEL + 1];
 
+		SkipListNode<K, V>* p = head;
+
+		//遍历找到每层要更新的节点
+		for (int i = level; i >= 0; i--)
+		{
+			while (p->forward[i]->key < key)
+			{
+				p = p->forward[i];
+			}
+
+			//将当前层要更新的节点放入update
+			updata[i] = p;
+		}
+
+		p = p->forward[i];
+
+		//若已存在则修改value
+		if (p->key == key)
+		{
+			p->value = value;
+		}
+
+		//获取新节点的最大层数
+		int max_level = randomLevel();
+
+		//如果大于当前最大层数
+		if (max_level > level)
+		{
+			max_level = ++level;
+
+			//在新一层存放head
+			updata[max_level] = head;
+		}
+
+		//新建节点
+		SkipListNode<K, V>* newnode = new SkipListNode<K, V>(key, value, max_level);
+
+		//在0~level层插入新节点
+		for (int i = level; i >= 0; i--)
+		{
+			//插入到p后面
+			p = updata[i];
+
+			newnode->forward[i] = p->forward[i];
+			p->forward[i] = newnode;
+		}
+
+		//增加跳表长度
+		++length;
+	}
+
+	//删除键值为 key 的节点,返回是否删除成功
+	//过程和插入相似，不过为删除
+	bool erase(const K& key)
+	{
+		//用于记录需要更新的节点
+		SkipListNode<K, V>* updata[MAX_LEVEL + 1];
 
 		SkipListNode<K, V>* p = head;
 
+		//遍历找到每层要更新的节点
+		for (int i = level; i >= 0; i--)
+		{
+			while (p->forward[i]->key < key)
+			{
+				p = p->forward[i];
+			}
+
+			//将当前层要更新的节点放入update
+			updata[i] = p;
+		}
+
+		p = p->forward[i];
+
+		//如果节点不存在
+		if (p->key != key)
+		{
+			return false;
+		}
+
+		//从最底层开始删除
+		for (int i = 0; i <= level; i++)
+		{
+			// 如果这层没有 p 删除就完成了
+			if (update[i]->forward[i] != p) {
+				break;
+			}
+			// 断开 p 的连接
+			update[i]->forward[i] = p->forward[i];
+		}
+
+		//回收删除节点的空间
+		delete p;
+
+		// 删除节点可能导致最大层数减少
+		while (level > 0 && head->forward[level] == tail) --level;
+
+		// 跳表长度
+		--length;
+
+		return true;
 	}
 };
 
